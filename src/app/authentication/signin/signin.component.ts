792a21307e6ead8634514ce33b5b191fdba98ab3
HammamN <hammamn@thevtool.com>
2024-05-21 18:07:21 +0300

    [#4] remove user rol and default fields texts on signin.component.ts.

--- src/app/authentication/signin/signin.component.ts
+++ src/app/authentication/signin/signin.component.ts
@@ -43,8 +43,8 @@ export class SigninComponent
 
   ngOnInit() {
     this.authForm = this.formBuilder.group({
-      username: ['admin@hospital.org', Validators.required],
-      password: ['admin@123', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
     });
   }
   get f() {
