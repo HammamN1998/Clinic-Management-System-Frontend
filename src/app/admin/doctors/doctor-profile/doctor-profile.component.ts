de4d3603b94625878a3856b375083cc8b302b110
HammamN <hammamn@thevtool.com>
2025-01-17 14:05:26 +0200

    [#27] Hide Secretary settings from secretary account.

--- src/app/admin/doctors/doctor-profile/doctor-profile.component.ts
+++ src/app/admin/doctors/doctor-profile/doctor-profile.component.ts
@@ -10,7 +10,7 @@ import {FirebaseAuthenticationService} from "../../../authentication/services/fi
 import {MatTooltipModule} from "@angular/material/tooltip";
 import {EditableTextComponent} from "@shared/components/editable-text/editable-text.component";
 import {DoctorService} from "@core/service/doctor.service";
-import {User} from "@core";
+import {Role, User} from "@core";
 import {from} from "rxjs";
 import {NotificationService} from "@core/service/notification.service";
 import {UntypedFormBuilder, UntypedFormGroup, Validators} from "@angular/forms";
@@ -220,4 +220,5 @@ export class DoctorProfileComponent {
   }
 
   protected readonly isNullOrUndefined = isNullOrUndefined;
+  protected readonly Role = Role;
 }
