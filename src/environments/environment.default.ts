e92565b2eef96b662670a540dc36fc8e433b19c7
HammamN1998 <hammamjobs@gmail.com>
2026-05-13 18:24:47 +0300

    [Unify Firebase Regions][#56] update firestore, storage, and functons to use europe-west1 region

--- src/environments/environment.default.ts
+++ src/environments/environment.default.ts
@@ -5,7 +5,7 @@ export const defaultEnvironment = {
     apiKey: "AIzaSyDL0xwhpno2HzkVQ1Ndm2CTf7O5DWknbbA",
     authDomain: "clinic-management-system-12-23.firebaseapp.com",
     projectId: "clinic-management-system-12-23",
-    storageBucket: "clinic-management-system-12-23.appspot.com",
+    storageBucket: "europe-west1-storage-1",
     messagingSenderId: "71169558916",
     appId: "1:71169558916:web:9267d1a20873878f0335d1",
     measurementId: "G-4ZTE50TCLY"
