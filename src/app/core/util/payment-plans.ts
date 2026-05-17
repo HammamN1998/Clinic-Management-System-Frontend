db66b4afae01e617f42672de0584580d42684ea5
HammamN1998 <hammamjobs@gmail.com>
2026-05-17 12:32:09 +0300

    [Website Subscriptions][#28] Update Plans prices.

--- src/app/core/util/payment-plans.ts
+++ src/app/core/util/payment-plans.ts
@@ -4,10 +4,10 @@ export const PAYMENT_PLANS = [
     name: 'Basic Plan',
     description: 'Up to 2000 patients.\nUp to 5 GB of storage.',
     prices: [
-      { type: 'renewable', duration: 'Monthly', price: 15, description: '$15 per month', priceId: 'price_1SJWlqQ9YgX6ekXrvHEsDSF9' },
-      { type: 'renewable', duration: 'Yearly', price: 144, description: '$144 per year', priceId: 'price_1TQYexQ9YgX6ekXrofQDmLlB' },
-      { type: 'non-renewable', duration: 'One Month', price: 20, description: '$20 for one month', priceId: 'price_1TRXLUQ9YgX6ekXrwF32kcaz' },
-      { type: 'non-renewable', duration: 'One Year', price: 240, description: '$240 for one year', priceId: 'price_1TRxzRQ9YgX6ekXrCrNO5QrE' },
+      { type: 'renewable', duration: 'Yearly', price: 180, encourage: 'Save $24/year', priceId: 'price_1TXztvQ9YgX6ekXrvEWItWdG' },
+      { type: 'renewable', duration: 'Monthly', price: 17, encourage: '', priceId: 'price_1TXzLSQ9YgX6ekXrLtOWm5vZ' },
+      { type: 'non-renewable', duration: 'One Year', price: 220, encourage: '', priceId: 'price_1TY01FQ9YgX6ekXrNcfiblDT' },
+      { type: 'non-renewable', duration: 'One Month', price: 20, encourage: '', priceId: 'price_1TRXLUQ9YgX6ekXrwF32kcaz' },
     ],
   },
   {
@@ -15,10 +15,10 @@ export const PAYMENT_PLANS = [
     name: 'Pro Plan',
     description: 'Up to 6000 patients.\nUp to 25 GB of storage.',
     prices: [
-      { type: 'renewable', duration: 'Monthly', price: 39, description: '$39 per month', priceId: 'price_1TRf21Q9YgX6ekXrcuVQYGZN' },
-      { type: 'renewable', duration: 'Yearly', price: 396, description: '$396 per year', priceId: 'price_1TRf2bQ9YgX6ekXrYpwNe4vg' },
-      { type: 'non-renewable', duration: 'One Month', price: 44, description: '$44 for one month', priceId: 'price_1TRf31Q9YgX6ekXrR0R9FWof' },
-      { type: 'non-renewable', duration: 'One Year', price: 528, description: '$528 for one month', priceId: 'price_1TRy62Q9YgX6ekXrH4nHs4nb' },
+      { type: 'renewable', duration: 'Yearly', price: 420, encourage: 'Save $48/year', priceId: 'price_1TRf2bQ9YgX6ekXrYpwNe4vg' },
+      { type: 'renewable', duration: 'Monthly', price: 39, encourage: '', priceId: 'price_1TRf21Q9YgX6ekXrcuVQYGZN' },
+      { type: 'non-renewable', duration: 'One Year', price: 500, encourage: '', priceId: 'price_1TRy62Q9YgX6ekXrH4nHs4nb' },
+      { type: 'non-renewable', duration: 'One Month', price: 44, encourage: '', priceId: 'price_1TRf31Q9YgX6ekXrR0R9FWof' },
     ],
   },
 ];
